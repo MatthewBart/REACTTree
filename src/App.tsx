40729fccd7140a640e53b0bc57967087d0492b50
MatthewBart <MatthewCBartley@gmail.com>
2022-11-25 11:17:56 +0000

    Basic set up for looping through files

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type TypeEntry={
+  name: string;
+  children?: TypeEntry[];
+}
+const files = {
+  children: [
+    {
+      name: "node_modules",
+      children:[
+        {
+          name: "joi",
+        }
+      ]
+    },
+    {
+      name: "package.json",
+    },
+    {
+      name: "vite.config.ts"
+    },
+  ],
+}
+function Entry({name,children}: TypeEntry) {
+  return <div>{name}</div>
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
+      {files.children.map((entry) => (
+        <Entry {...entry}/>
+      ))};
     </div>
   );
 }
